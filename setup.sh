79914e307bd57178763e79aa2709da2bb0c56179
Miguel Nascimento dos Santos <miguel.nasto@gmail.com>
2024-08-24 14:26:38 -0300

    feat: add git configuration to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
 #Installing git, code and zsh
-echo "Installing git, code & zsh..."
+echo -e "Installing git, code & zsh...\n"
 sudo pacman -Syu --noconfirm
 sudo pacman -S --noconfirm git code zsh
 
+#Git configuration 
+echo -e "Configuring git..."
+git config --global user.email = "miguel.nasto@gmail.com"
+git config --global user.name = "Miguel Nascimento dos Santos"
+git config --global --list
+
 #Configuring oh-my-zsh
-echo "Configuring oh-my-zsh..."
+echo -e "Configuring oh-my-zsh...\n"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Moving files
-echo "Moving files..."
+echo -e "Moving files...\n"
 mv ./files/.zshrc ~/
 mv ./files/comppile.sh ~/.local/bin/
 
+echo -e "Installation complete! Rebooting...\n"
+
 chsh -s $(which zsh)
 source ~/.zshrc
-
-echo "Installation complete!"
 
 for i in {3..1}; do
     echo "$i..."
