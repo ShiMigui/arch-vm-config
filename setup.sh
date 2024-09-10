880504fda19714f78fa705ac7eaf056bb8b326b3
Miguel Nascimento dos Santos <miguel.nasto@gmail.com>
2024-09-09 21:23:51 -0300

    setting the news default programs

--- setup.sh
+++ setup.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
 
-#Installing git, code and zsh
-echo -e "Installing git, code & zsh...\n"
-sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm git code zsh
+# Installing git, code, and zsh
+echo -e "Installing programs...\n"
+sudo pacman -S --noconfirm qutebrowser zsh neovim > /dev/null 2>&1
 
-#Git configuration 
+# Git configuration 
 echo -e "Configuring git..."
-git config --global user.email = "miguel.nasto@gmail.com"
-git config --global user.name = "Miguel Nascimento dos Santos"
+git config --global user.email "miguel.nasto@gmail.com"
+git config --global user.name "Miguel Nascimento dos Santos"
 git config --global --list
 
-#Configuring oh-my-zsh
+# Configuring oh-my-zsh
 echo -e "Configuring oh-my-zsh...\n"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# Moving files
-echo -e "Moving files...\n"
-mv ./files/.zshrc ~/
-mv ./files/comppile.sh ~/.local/bin/
+# Install Oh My Zsh without auto-execution
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
 
-echo -e "Installation complete! Rebooting...\n"
+# Copy .zshrc to home directory
+echo "Copying .zshrc to home directory..."
+cp ./.zshrc ~/
 
-chsh -s $(which zsh)
+# Source the new .zshrc to apply changes
+echo "Sourcing .zshrc to apply changes..."
 source ~/.zshrc
 
-for i in {3..1}; do
-    echo "$i..."
-    sleep 1
-done
-sudo reboot now
+echo "Setup complete!"
